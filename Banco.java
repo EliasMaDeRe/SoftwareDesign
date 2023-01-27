c950e5f51c1b0584e2864f8f7f33ea08633cd8f7
Elias Madera <a18000577@alumnos.uady.mx>
2023-01-27 11:23:51 -0600

    Inicial

--- Banco.java
+++ Banco.java
@@ -1,30 +1,74 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Banco {
 
-    public void imprimirClientes(String archivo){
+    private String archivo = "";
 
-        try{
-            File file = new File(archivo+".txt");
-            Scanner scanner = new Scanner(file);
-            String linea = new String();
-            while(scanner.hasNextLine()){
-                linea = scanner.nextLine();
-                String[] datos = linea.split(",");
-                for(int i = 0; i < datos.length; ++i){
-                    System.out.print(datos[i]+" ");
-                    
-                }
-                System.out.println();
-            }
-        }catch(Exception e){
+    Banco(String archivo){
 
-            System.out.println("No se pudo abrir el archivo");
+        this.archivo = archivo;
 
+    }
+
+    public String getArchivo(){
+
+        return this.archivo;
+
+    }
+
+    public void imprimirClientes(){
+
+        ArrayList<ArrayList<String>> clientes = obtenerClientes();
+
+        for(int i = 0; i < clientes.size(); ++i){
+
+            for(int j = 0; j < clientes.get(i).size(); ++j)
+                System.out.print(clientes.get(i).get(j)+" ");
+            System.out.println();
         }
 
     }
 
-    
+    public ArrayList<ArrayList<String>> obtenerClientes(){
+
+        ArrayList<ArrayList<String>> clientes = new ArrayList<>();
+        try{
+            File file = new File(archivo+".txt");
+            Scanner scanner = new Scanner(file);
+            
+            String linea;
+            while(scanner.hasNextLine()){
+                linea = scanner.nextLine();
+                ArrayList<String> datos =  new ArrayList<String>(Arrays.asList(linea.split(",")));
+                clientes.add(datos);
+            }
+            scanner.close();
+        }catch(Exception e){
+
+            System.out.println("No se pudo abrir el archivo");
+
+        }       
+        return clientes;
+    }
+
+    public void anadirCliente(String[] args, BufferedWriter write) throws IOException{
+        
+        String linea = "";
+        write.newLine();
+        for(int i = 0; i < args.length; ++i){
+
+            linea+=args[i];
+            if(i != args.length-1)
+                linea+=",";
+
+        }
+        write.write(linea);
+        write.close();
+
+    }
 }
--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Banco banco = new Banco();
-        banco.imprimirClientes("clientes");
+        Controlador controlador = new Controlador("clientes");
+        controlador.getBanco().imprimirClientes();
+        String[] cliente = new String[]{"001","Elias Madera De Regil","1234567890123443","129"};
+        System.out.println(controlador.anadirCliente(cliente));
+        
 
     }
     
